7934d5aba70be5b48704bb942d993871b0beed05
Amit Singh <amit21671@gmail.com>
2023-03-27 00:38:11 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+// script.js
+//const fs = require('fs')
+let arr=[];
+const myDiv=window.document.querySelectorAll('h3');
+for(let i=0;i<myDiv.length;i++){
+   // let obj={"Ques":""};
+    
+    //console.log(myDiv[i].outerText)
+    //obj.Ques=myDiv[i].outerText
+    //arr.push(myDiv[i].outerText);
+}
+//console.log(arr);
+
+const myDiv1=window.document.querySelectorAll('span');
+//console.log(myDiv1[2].innerText)
+for(let i=0;i<myDiv.length;i++){ 
+     console.log(myDiv1[i].innerText)
+     //obj.Ques=myDiv[i].outerText
+     //arr.push(myDiv[i].outerText);
+ }
+
+
+
+// Object.values(arr);
+// fs.writeFile("output.txt", str, "utf-8", (error) => {
+  
+//     // If there is error then this will execute
+//     if (error) {
+//       console.log(error);
+//     }
+//     else {
+//       console.log("Successfully written!!");
+//     }
+      
+//     // Reading what we have written in file
+//     fs.readFile("output.txt", "utf-8", (error, data) => {
+//       if (error) {
+//         console.log(error);
+//       } 
+//       else {
+//         console.log(data);
+//       }
+//     });
+//   });
+
+
+
+
+
